23fc9394f7cc8af0bc62453ac7673b395ee1b3ad
mdkutbul <mdkutbul.wara@gmail.com>
2023-02-17 09:33:38 -0500

    Add files via upload

---
+++ glue_name.py
@@ -0,0 +1,15 @@
+# FILE NAME - glue_name.py
+# NAME - Md Wara
+# DATE - 02-08-2023
+# DISCRIPTION - outputs first name and last name in one line
+
+def main():
+    glue_name()
+
+def glue_name():
+    FirstName = (input ("Enter the First Name: ") )
+    LastName = (input ("Enter the Last Name: "))
+    print("Hi,", FirstName, LastName )
+
+    
+main()

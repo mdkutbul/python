abb9d1bd6e91dc5219c26433d3cab00ce13b0da8
mdkutbul <mdkutbul.wara@gmail.com>
2023-02-17 09:32:18 -0500

    Add files via upload

---
+++ randomNumber.py
@@ -0,0 +1,25 @@
+# FILE NAME - randomNumber.py
+
+# NAME - Md Wara
+# DATE - 02/16/2023
+# DESCRIPTION - generates 3 random numbers
+
+import random
+
+
+def main():
+    generateRandomNumber()
+
+def generateRandomNumber():
+ num1 = random.random()
+ num2 = random.random()
+ num3 = random.random()
+
+ print(num1)
+ print(num2)
+ print(num3)
+
+
+
+
+main()
